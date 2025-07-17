f7e19f10f779e0d0ffaba6857b81c89f7f872e69
Rafael Guibson <rafaelguibsons@gmail.com>
2025-07-17 20:12:25 -0300

    Modelagem das entidades Task e Category, migrations ok

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,2 +1,48 @@
 from django.db import models
+from django.conf import settings
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+
+    class Meta:
+        unique_together = ('name', 'owner')
+
+    def __str__(self):
+        return f'{self.name} ({self.owner.username})'
+
+    
+    
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tasks',
+        verbose_name='Tarefas'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='tasks',
+        verbose_name='Tarefas'
+        
+    )
+    shared_with = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='shared_tasks',
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.title
