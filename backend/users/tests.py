6c401d70c38286280d6b560d301d979bd62013c9
Rafael Guibson <rafaelguibsons@gmail.com>
2025-07-17 16:51:03 -0300

    criado app de user customizado e implementado arquivo de teste da app

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+@pytest.mark.django_db
+def test_create_user():
+    user = User.objects.create_user(username='testuser', password='password123')
+    assert user.username == 'testuser'
+    assert user.is_active
+    assert not user.is_staff
+    assert not user.is_superuser
+    #Verificar se a senha foi hadheada
+    assert user.check_password('password123')
+    assert not user.check_password('wrongpassword')
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
