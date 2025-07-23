09ea8e6e0144f654836e1c00efe53d25bea87019
Rafael Guibson <137908322+rafaelguibson@users.noreply.github.com>
2025-07-22 22:45:50 -0300

    Update App.jsx
    Atualização de rota da pagina inicial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Tasks from "./pages/Tasks";
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
